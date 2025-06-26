a4c8b52a0b3251de163864708dc8227cb55fdbcd
Del Sharratt <delaney.sharratt@gmail.com>
2025-06-26 16:13:01 -0500

    refactor: new generic component form + layout form for improved abstraction

--- components/shared/ui/ComponentForm.tsx
+++ components/shared/ui/ComponentForm.tsx
@@ -1,28 +1,43 @@
-import FormFieldRenderer, { FormField } from '@/components/shared/form/FormField';
+'use client';
 
-export default function Form({ formFields }: { formFields: FormField[] }) {
-  const dropdownFields = formFields.filter((field) => field.type === 'dropdown');
-  const toggleFields = formFields.filter((field) => field.type === 'toggle');
+import React, { useState } from 'react';
 
-  return (
-    <div className="flex flex-row gap-4 w-full border-b border-neutral-400 pb-10">
-      <FormColumnPanel size="2/3" fields={dropdownFields} />
-      <FormColumnPanel size="1/3" fields={toggleFields} />
-    </div>
-  );
+import { FORM_FIELD_IDS, FormField } from '@/components/shared/form/FormField';
+import Form from '@/components/shared/layout/Form';
+import { ComponentRegistryEntry } from '@/core/system/componentRegistry';
+import { Library } from '@/core/system/uiLibraries';
+
+export interface ComponentFormProps extends ComponentRegistryEntry {
+  library: Library;
 }
 
-function FormColumnPanel({
-  fields,
-  size
-}: {
-  size: '1/3' | '2/3';
-  fields: FormField[];
-  className?: string;
-}) {
+export default function GenericComponentForm({
+  library,
+  componentMap,
+  formConfigMap,
+  staticData = {}
+}: ComponentFormProps) {
+  const Component = componentMap[library];
+  const formConfig = formConfigMap[library];
+
+  const initialState = Object.fromEntries(formConfig.map((field) => [field.prop, field.default]));
+  const [formState, setFormState] = useState(initialState);
+
+  const fields: FormField[] = formConfig.map((field) => ({
+    ...field,
+    value: formState[field.prop ?? ''],
+    onChange: (e) => {
+      const value = field.type === FORM_FIELD_IDS.Toggle ? e.target.checked : e.target.value;
+      setFormState((prev: any) => ({ ...prev, [field.prop ?? '']: value }));
+    }
+  }));
+
   return (
-    <div className={`flex flex-col ${size === '1/3' ? 'basis-1/3' : 'basis-2/3'} gap-4`}>
-      <FormFieldRenderer fields={fields} />
-    </div>
+    <>
+      <Form formFields={fields} />
+      <div className="flex justify-around mt-8 w-full">
+        <Component {...formState} {...staticData} />
+      </div>
+    </>
   );
 }
